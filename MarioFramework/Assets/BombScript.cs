0bb270a1a48b68c248acbc001521f81a8b0293af
Ben Johnson <ben.ruby3@gmail.com>
2016-11-23 10:58:16 +0000

    Event triggers experimentation; Fix needed for functionality

---
+++ MarioFramework/Assets/BombScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombScript : MonoBehaviour {
+    ParticleSystem bombSyst;
+    GameObject self;
+	// Use this for initialization
+	void OnTriggerEnter () {
+        bombSyst.Play();
+       
+       // self.a
+	}
+	
+	
+}
